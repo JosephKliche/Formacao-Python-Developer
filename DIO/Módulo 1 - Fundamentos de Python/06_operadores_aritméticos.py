c253f97ba6a819738253bc72912d4ef7b4884587
JosephKliche <126715267+JosephKliche@users.noreply.github.com>
2024-01-10 20:35:28 -0300

    Create 06_operadores_aritméticos.py

---
+++ DIO/Módulo 1 - Fundamentos de Python/06_operadores_aritméticos.py
@@ -0,0 +1,45 @@
+# Adição
+print (1 + 1)
+>>> 2
+
+# Subtração
+print (10 - 2)
+>>> 9
+
+# Multiplicação
+print (4 * 3)
+>>> 12
+
+# Divisão
+print (12 / 3)
+>>> 4.0
+
+# Divisão inteira
+print (12 // 2)
+>>> 6
+
+# Módulo - resto da divisão
+print (10 % 3)
+>>> 1
+
+# Exponenciação
+print (2 ** 3)
+>>> 8
+
+# outros testes
+
+produto_1 = 10
+produto_2 = 20
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_2 / produto_1)
+print(produto_2 // produto_1)
+print(produto_1 * produto_2)
+print(produto_1 % produto_2)
+print(produto_1 ** produto_2)
+
+x = (10 + 5) + 4
+y = 10 / 2 + 25 * 2 - 2 **2
+print(x)
+print(y)
