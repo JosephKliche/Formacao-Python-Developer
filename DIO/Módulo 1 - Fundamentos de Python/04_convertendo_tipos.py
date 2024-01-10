7cf56318fd316e490d250684715f7a0a0e7fc790
JosephKliche <126715267+JosephKliche@users.noreply.github.com>
2024-01-10 19:33:24 -0300

    Update and rename convetendo_tipos.py to 04_convertendo_tipos.py

---
+++ DIO/Módulo 1 - Fundamentos de Python/04_convertendo_tipos.py
@@ -0,0 +1,26 @@
+#inteiro para float
+preco = 10
+print(preco)
+>>> 10
+
+preco = float(preco)
+print(preco)
+>>> 10.0
+
+preco = 10 / 2
+print(preco)
+>>> 5.0
+
+#outras operações
+print(int(1.9))
+print(int("10"))
+print(float("10.10"))
+print(float(100))
+
+valor = 10
+valor_str = 10.10
+print(type(valor))
+print(type(valor_str))
+
+print (100 / 2)
+print (100 // 2)
