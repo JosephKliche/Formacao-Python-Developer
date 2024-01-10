6b3c3458a3ba7c06e0b4f630f54aea98f27ef70e
JosephKliche <126715267+JosephKliche@users.noreply.github.com>
2024-01-10 20:42:16 -0300

    Create 07_operadores_de_comparacao.py

---
+++ DIO/Módulo 1 - Fundamentos de Python/07_operadores_de_comparacao.py
@@ -0,0 +1,33 @@
+# Igualdade
+
+saldo = 450
+saque = 200
+
+print(saldo += saque)
+>>> False
+
+# Diferença
+
+saldo = 450
+saque = 200
+
+print(saldo != saque)
+>>> True
+
+# Maior que / maior ou igual
+
+saldo = 450
+saque = 200
+print (saldo > saque)
+>>> True
+
+print (saldo >= saque)
+
+# Menor que / menor ou igual
+
+saldo = 450
+saque = 200
+print (saldo < saque)
+>>> True
+
+print (saldo <= saque)
