e00eda45e51017b16b93a9bfc53e7aea8c3442ee
JosephKliche <126715267+JosephKliche@users.noreply.github.com>
2024-01-10 19:13:12 -0300

    Create variaveis_e_constantes.py

---
+++ DIO/Módulo 1 - Fundamentos de Python/variaveis_e_constantes.py
@@ -0,0 +1,21 @@
+age = 28
+name = 'Guilherme'
+print(f'Meu nome é {name} e eu tenho {age} ano(s) de idade.')
+#>>>>> Meu nome é Guilherme e eu tenhos 28 ano(s) de idade.
+  
+age = 27
+name = 'Giovanna'
+print(f'Meu nome é {name} e eu tenho {age} ano(s) de idade.')
+#>>>>> Meu nome é Giovanna e eu tenhos 27 ano(s) de idade.
+
+#Em Python não temos palavras reservadas, ou constantes, por isso usamos a convenção que diz ao programador
+#que a variável é uma constante. Para fazer isso, deve-se utilizar a variável com o nome todo em letras maíuscula.
+
+ABS_PATH = '/home/guilherme/Documents/python_couse/'
+DEBUG = True
+STATES = [
+  'SP',
+  'RJ',
+  'ES,
+]
+AMOUNT = 30.2
