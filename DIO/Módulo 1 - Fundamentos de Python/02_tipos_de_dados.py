8e9826169ea22abfa5d1349ea6661c386d8901f2
JosephKliche <126715267+JosephKliche@users.noreply.github.com>
2024-01-10 18:45:23 -0300

    Update 02_tipos_de_dados.py

--- DIO/Módulo 1 - Fundamentos de Python/02_tipos_de_dados.py
+++ DIO/Módulo 1 - Fundamentos de Python/02_tipos_de_dados.py
@@ -1,5 +1,10 @@
-print(11 + 10 + 1000)
-print(1.5 + 1 + 0.5)
-print(True)
-print(False)
-print("Python")
+print(11 + 10 + 1000)  #tipo inteiro
+print(1.5 + 1 + 0.5)   #tipo float
+print(True)            #tipo boleano
+print(False)           #tipo boleano
+print("Python")        #tipo string
+
+int()
+float()
+str()
+bool()
