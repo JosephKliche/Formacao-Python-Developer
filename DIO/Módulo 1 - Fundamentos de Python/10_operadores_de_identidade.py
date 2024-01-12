64b8cb3e42b34f7a2175ca4e38c053158406a071
JosephKliche <126715267+JosephKliche@users.noreply.github.com>
2024-01-12 20:58:53 -0300

    Add files via upload

---
+++ DIO/Módulo 1 - Fundamentos de Python/10_operadores_de_identidade.py
@@ -0,0 +1,13 @@
+# Operador IS ou IS NOT
+curso = "Curso de Python"
+nome_curso = curso
+saldo, limite = 200, 200
+
+curso is nome_curso
+# >>> True
+
+curso is not nome_curso
+# >>> False
+
+saldo is limite
+# >>> True
