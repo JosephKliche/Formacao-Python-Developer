24cddcf4fad50bf45670c8de6bb8b743af29c814
JosephKliche <126715267+JosephKliche@users.noreply.github.com>
2024-01-12 20:29:06 -0300

    Add files via upload

---
+++ DIO/Módulo 1 - Fundamentos de Python/operadores_de_atribuicao.py
@@ -0,0 +1,50 @@
+# Atribuição simples
+saldo = 500
+
+# Atribuição com adição
+saldo = 500
+saldo += 500
+
+print(saldo)
+>>> 1000
+
+# Atribuição com subtração
+saldo = 500
+saldo -= 500
+
+print(saldo)
+>>> 0
+
+# Atribuição com multiplicação
+saldo = 500
+saldo *= 2
+
+print(saldo)
+>>> 1000
+
+# Atribuição com divisão
+saldo = 500
+saldo /= 5
+
+print(saldo)
+>>> 100.0
+
+saldo = 500
+saldo //= 5
+
+print(saldo)
+>>> 100
+
+# Atribuição com módulo
+saldo = 500
+saldo %= 480
+
+print (saldo)
+>>> 20
+
+# Atribuição com exponenciação
+saldo = 80
+saldo **= 2
+
+print(saldo)
+>>> 6400
