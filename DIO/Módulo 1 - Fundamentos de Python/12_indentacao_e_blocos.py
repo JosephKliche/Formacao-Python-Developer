abb4c0345e2fc32625f317a8baf6d3c399c1a9b6
José Daniel <126715267+josedanielgadioli@users.noreply.github.com>
2024-01-13 15:34:42 -0300

    Add files via upload

---
+++ DIO/Módulo 1 - Fundamentos de Python/12_indentacao_e_blocos.py
@@ -0,0 +1,10 @@
+# Bloco em Python
+def sacar(self, valor: float) -> None : # início do bloco do método
+
+    if self.saldo >= valor: # início do bloco do if
+
+        self.saldo -= valor
+    
+    # fim do bloco do if
+        
+# fim do bloco do método
