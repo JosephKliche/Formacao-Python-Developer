40c7423c9b540a45e339437877e39de7575c7840
José Daniel <126715267+josedanielgadioli@users.noreply.github.com>
2024-01-22 18:20:35 -0300

    Add files via upload

---
+++ DIO/Módulo 2 - Apresentando Estruturas de Dados com Python/03_tuplas.py
@@ -0,0 +1,17 @@
+# Exemplo
+
+frutas = ("laranja", "pera", "uva",)
+
+letras = tuple("python")
+
+numeros = tuple([1, 2, 3, 4])
+
+pais = ("Brasil",)
+
+frutas = ("maçã", "laranja", "uva", "pera",)
+frutas[0] # maçã
+frutas[2] # uva
+
+frutas = ("maçã", "laranja", "uva", "pera",)
+frutas[-1] # pera
+frutas[-3] # laranja
